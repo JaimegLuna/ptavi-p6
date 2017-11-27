b90250d0a133c306bfb849b57c44e845595fa0d1
Jaime Garcia Luna <jluna@alpha27.aulas.gsyc.urjc.es>
2017-11-27 10:43:54 +0100

    añadidos methods

--- server.py
+++ server.py
@@ -5,7 +5,8 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
-import sys
+import sys 
+import os
 
 
 class EchoHandler(socket.server.DatagramRequestHandler):
@@ -22,4 +23,29 @@ def handle(self):
 		self.wfile.write(b"SIP/2.0 180 Ring\r\n\r\n")
 		self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
 	
+	elif METHOD == 'ACK'
+		aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + audio)
+		print("Executing...", aEjecutar)
+		print()
+		os.system(aEjecutar)
+
+	elif METHOD == 'BYE':
+		self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+
+	
 if __name__=="main__":
+	
+	try:
+		IP_s = sys.argv[1]
+		PORT_s = int(sys.argv[2])
+		audio = sus.argv[3]
+
+	try:
+		serv.serve_forever()
+	except KeyboardInterrupt:
+		print()
+		print("Finalizado Servidor")
+		
+
+
+
