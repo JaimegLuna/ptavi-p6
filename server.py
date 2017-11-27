f3e083f17419fff014a3ab66c88fd6b1f510bcfe
Jaime Garcia Luna <jluna@alpha27.aulas.gsyc.urjc.es>
2017-11-27 10:22:52 +0100

    metodo INVITE

--- server.py
+++ server.py
@@ -1,66 +1,25 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Clase (y programa principal) para un servidor de eco en UDP simple
+Clase (y programa principal) para un servidor de eco en UDP simple 
 """
 
 import socketserver
 import sys
-import os
 
 
-class EchoHandler(socketserver.DatagramRequestHandler):
-    """
-    Echo server class
-    """
+class EchoHandler(socket.server.DatagramRequestHandler):
+	"""
+	Echo server class
+	"""
+def handle(self):
 
-    def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
-
-        # Leyendo línea a línea lo que nos envía el cliente
-        line = self.rfile.read()
-        data = line.decode('utf-8')
-        plin = data.split(' ')
-        METHOD = plin[0]
-
-        # Detección de método SIP
-        if METHOD == 'INVITE':
-            print("El cliente nos manda: " + data)
-            self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n")
-            self.wfile.write(b"SIP/2.0 180 Ring\r\n\r\n")
-            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-
-        elif METHOD == 'ACK':
-            print("El cliente nos manda: " + data)
-            aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + audio
-            print("Executing...", aEjecutar)
-            print()
-            os.system(aEjecutar)
-
-        elif METHOD == 'BYE':
-            print("El cliente nos manda: " + data)
-            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-
-        elif METHOD not in ['INVITE', 'ACK', 'BYE']:
-            self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
-        else:
-            self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
-
-
-if __name__ == "__main__":
-    # Creamos servidor de eco y escuchamos
-    try:
-        IP_S = sys.argv[1]
-        PORT_S = int(sys.argv[2])
-        audio = sys.argv[3]
-    except:
-        sys.exit("Usage: python3 server.py IP port audio_file")
-
-    serv = socketserver.UDPServer(('', PORT_S), EchoHandler)
-    print("Listening...")
-
-    try:
-        serv.serve_forever()
-    except KeyboardInterrupt:
-        print()
-        print("Finalizado servidor")
+	"""
+	Metodos
+	"""
+	if METHOD == 'INVITE':
+		self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n")
+		self.wfile.write(b"SIP/2.0 180 Ring\r\n\r\n")
+		self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+	
+if __name__=="main__":
