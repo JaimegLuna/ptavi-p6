7ef4342ca63a2ae68c0f1cb233e0b151301ce25b
Jaime Garcia Luna <jluna@alpha27.aulas.gsyc.urjc.es>
2017-11-27 11:02:04 +0100

    fin servidor

--- server.py
+++ server.py
@@ -15,23 +15,34 @@ class EchoHandler(socket.server.DatagramRequestHandler):
 	"""
 def handle(self):
 
+	line = self.rfile.read()
+	data = line.decode.('utf-8')
+	plin = data.split(' ')
+	METHOD = plin[0]
 	"""
 	Metodos
 	"""
 	if METHOD == 'INVITE':
+		print("El cliente manda: " + data)
 		self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n")
 		self.wfile.write(b"SIP/2.0 180 Ring\r\n\r\n")
 		self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
 	
 	elif METHOD == 'ACK'
+		print("El cliente manda: " + data)
 		aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + audio)
 		print("Executing...", aEjecutar)
 		print()
 		os.system(aEjecutar)
 
 	elif METHOD == 'BYE':
+		print("El cliente nos manda: " + data)
 		self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
 
+	elif METHOD not in ['INVITE', 'ACK', 'BYE']:
+		self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
+	else:	
+		self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
 	
 if __name__=="main__":
 	
@@ -39,13 +50,14 @@ if __name__=="main__":
 		IP_s = sys.argv[1]
 		PORT_s = int(sys.argv[2])
 		audio = sus.argv[3]
+	except:
+		sys.exit("Usage: python3 server.py IP port audio_file")
+
+	serv = socketserver.UDPserver(('',PORT_s), EchoHandler)
+	print('Listening')
 
 	try:
 		serv.serve_forever()
 	except KeyboardInterrupt:
 		print()
 		print("Finalizado Servidor")
-		
-
-
-
